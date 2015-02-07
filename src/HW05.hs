f2f2ffffc33c2a738fad1853f688ea9b5537febf
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-02-07 14:24:29 -0800

    Proved first case

--- src/HW05.hs
+++ src/HW05.hs
@@ -8,8 +8,11 @@ Prove:  map f x ++ map f y = map f (x ++ y)
 -- Definitions and equations
 1 map f [] = []
 2 map f (x:xs) = (f x) : (map f xs)
+3 [] ++ x  = x
+4 x ++ []  = x
+5 [] ++ [] = []
 
-Prove
+-- Must Prove
 
 1) map f [] ++ map f [] = map f ([] ++ [])
 
@@ -17,12 +20,23 @@ Prove
 
 3) map f x ++ map f [] = map f (x ++ [])
 
-4)
+4) Assuming:    map f qs ++ map f vs = map f (qs ++ vs)
+   Prove:       map f (q:qs) ++ map f (v:vs) = map f ((q:qs) ++ (v:vs))
 
-4) map f bottom ++ map f bottom = map f (bottom ++ bottom)
+5) map f bottom ++ map f bottom = map f (bottom ++ bottom)
 
-5) map f bottom ++ map f y = map f (bottom ++ y)
+6) map f bottom ++ map f y = map f (bottom ++ y)
 
-6) map f x ++ map f bottom = map f (x ++ bottom)
+7) map f x ++ map f bottom = map f (x ++ bottom)
+
+-- Proof
+1) map f [] ++ map f [] = map f ([] ++ [])
+
+   map f [] ++ map f [] = map f []        -->   (By 5)
+   map f [] ++ map f [] = []              -->   (By 1)
+   [] ++ [] = []                          -->   (By using 1 twice)
+   [] = []                                -->   (By 5)
+
+2)
 
 -}
