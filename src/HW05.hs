1fd30c191c3b2071e8f5c8a50812cf86e9a22e16
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-02-10 07:34:00 -0800

    Started work on a worksheet and continue work on hw5

--- src/HW05.hs
+++ src/HW05.hs
@@ -8,46 +8,36 @@ Prove:  map f x ++ map f y = map f (x ++ y)
 -- Definitions and equations
 1 map f [] = []
 2 map f (x:xs) = (f x) : (map f xs)
-3 [] ++ x  = x
-4 x ++ []  = x
-5 [] ++ [] = []
+3 (x:xs) ++ ys = x : (xs ++ ys)
+4 [] ++ xs = xs
 
 -- Must Prove
 
-1) map f [] ++ map f [] = map f ([] ++ [])
+1) map f [] ++ map f y = map f ([] ++ y)
 
-2) map f [] ++ map f y = map f ([] ++ y)
+2) Assuming:    map f ps ++ map f y = map f (ps ++ y)
+   Prove:       map f (p:ps) ++ map f y = map f ((p:ps) ++ y)
 
-3) map f x ++ map f [] = map f (x ++ [])
-
-4) Assuming:    map f ps ++ map f vs = map f (qs ++ vs)
-   Prove:       map f (q:qs) ++ map f (v:vs) = map f ((q:qs) ++ (v:vs))
-
-5) map f bottom ++ map f bottom = map f (bottom ++ bottom)
-
-6) map f bottom ++ map f y = map f (bottom ++ y)
-
-7) map f x ++ map f bottom = map f (x ++ bottom)
+3) map f bottom ++ map f y = map f (bottom ++ y)
 
 -- Proof
-1) map f [] ++ map f [] = map f ([] ++ [])
+1) map f [] ++ map f y = map f ([] ++ y)
 
-   map f [] ++ map f [] = map f []        -->   (By 5)
-   map f [] ++ map f [] = []              -->   (By 1)
-   [] ++ [] = []                          -->   (By using 1 twice)
-   [] = []                                -->   (By 5)
-
-2) map f [] ++ map f y = map f ([] ++ y)
-
-   map f [] ++ map f y = map f y          -->   (By 3)
+   map f [] ++ map f y = map f y          -->   (By 4)
    [] ++ map f y = map f y                -->   (By 1)
-   map f y = map f y                      -->   (By 3)
+   map f y = map f y                      -->   (By 4)
 
-3) map f x ++ map f [] = map f (x ++ [])
+2) Assuming:    map f ps ++ map f y = map f (ps ++ y)
+   Prove:       map f (p:ps) ++ map f y = map f ((p:ps) ++ y)
 
-   map f x ++ map f [] = map f x          -->   (By 4)
-   map f x ++ [] = map f x                -->   (By 1)
-   map f x = map f x                      -->   (By 4)
+   (f p) : (map f ps) ++ map f y = map f ((p:ps) ++ y)      -->   (By 2)
+   (f p) : (map f ps ++ map f y) = map f ((p:ps) ++ y)      -->   (By 3)
+   (f p) : (map f ps ++ map f y) = map f ((p:ps) ++ y)      -->   (By 3)
+
+3) map f bottom ++ map f y = map f (bottom ++ y)
+
+   bottom ++ map f y = map f bottom    -->   (By pattern matching and strictness of map)
+   bottom = bottom                     -->   (By pattern matching)
 
 
 -}
